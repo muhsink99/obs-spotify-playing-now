2827783d83fa82acee62da36c1394e1f04a1eedb
muhsink99 <muhsinkumbay99@gmail.com>
2023-01-07 20:09:24 +0000

    fix: fix build errors with ' text

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,18 @@ import { authenticateWithSpotify } from "../services/spotify-service"
 
 export default function Home() {
   return (
-    <>
+    <div>
       <h3>READ BEFORE LOGGING IN</h3>
       <p>
-        This is currently not open to the public and is in development mode. You'll need to request access to be able to log in.
+        This is currently not open to the public and is in development mode. You&apos;ll need to request access to be able to log in.
         <br/>
-        Once you've been granted access, follow the steps below to set this up on your OBS client. 
+        Once you&apos;ve been granted access, follow the steps below to set this up on your OBS client. 
       </p>
       <ol>
         <li>Log into Spotify using the button below</li>
-        <li>You'll be redirected to a new page with your current song being played on Spotify. Keep a note of the entire URL</li>
-        <li>Create a new 'Browser' source on OBS. </li>
-        <li>Give this scene a size of 300x400 and give it the URL you just kept a note now.</li>
+        <li>You&apos;ll be redirected to a new page, showing the song you&apos;re currently playing on Spotify. Keep a note of the entire URL</li>
+        <li>Create a new &apos;Browser&apos; source on OBS. </li>
+        <li>Give this source a size of 300x400 and give it the URL you just kept a note of.</li>
         <li>Move the Browser source to your desired position in your current scene.</li>
       </ol>
       <h3>Known Issues</h3>
@@ -22,6 +22,6 @@ export default function Home() {
         <li>Song titles that are too long will cut off and mess up the styling</li>
       </ul>
       <button onClick={() => authenticateWithSpotify()}>Log into spotify</button>
-    </>
+    </div>
   )
 }
