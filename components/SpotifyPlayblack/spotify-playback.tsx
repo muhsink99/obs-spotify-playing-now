7404cabf0fd01a73668e7ca31bab1755e15fb1d9
loldcv <dcvtrash@gmail.com>
2023-01-13 00:22:05 +0100

    fix You provided a  prop to a form field without an  handler..

--- components/SpotifyPlayblack/spotify-playback.tsx
+++ components/SpotifyPlayblack/spotify-playback.tsx
@@ -12,7 +12,7 @@ export default function SpotifyPlayback(playbackData: any) {
                         <h2 className={styles.card__title}> {playbackData.data.songName} </h2>
                         <span className={styles.card__artist}> {playbackData.data.songArtist} </span>
                         <div className={styles.card__controllers}>
-                            <input type="range" value={playbackData.data.progressMs} max={playbackData.data.durationMs} id="tracker" />
+                            <input type="range" value={playbackData.data.progressMs} max={playbackData.data.durationMs} id="tracker" readOnly={true}/>
                             <span className={styles.card__controllers__time} id="current-time">{convertMillisecondsToTimestamp(playbackData.data.progressMs)}</span>
                             <span className={styles.card__controllers__time} id="info-time">{convertMillisecondsToTimestamp(playbackData.data.durationMs)} </span>
                         </div>
